df4135177926e3c8ccb639d2bc7af9de42d426ed
Z.Sun <zhaohuisun@foxmail.com>
2019-05-28 17:13:48 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+module.exports = function main(distance,parkTime) {
+    var summary=6;
+    if(distance<2)
+    {
+        summary=summary+0.25*parkTime;
+    }
+    else if(distance<8)
+    {
+        summary=summary+0.25*parkTime+0.8*(distance-2);
+    }
+    else
+    {
+        summary=summary+4.8+(distance-8)*1.2+0.25*parkTime;
+    }
+    return Math.round(summary);
 };
